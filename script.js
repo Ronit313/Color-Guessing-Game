d109797996921f2b229eded8376a3d3a820472e7
Ronit <ronit.mishra.met20@itbhu.ac.in>
2022-02-20 13:48:50 +0530

    first commit

---
+++ script.js
@@ -0,0 +1,93 @@
+const header = document.getElementsByClassName("header");
+const requiredColor = document.getElementById("requiredColor");
+const reset = document.getElementById("reset");
+const result = document.getElementById("message");
+const easy = document.getElementById("easybtn");
+const hard = document.getElementById("hardbtn");
+let button = document.getElementsByClassName("button");
+let color = [];
+let numSqaure = 6;
+requiredColor.style.fontSize = '60px';
+
+
+console.log(reset);
+
+generateRandomColors(numSqaure);
+setupSquare(numSqaure);
+
+function randomColor(){
+    let r = Math.floor(Math.random()*255);
+    let g = Math.floor(Math.random()*255);
+    let b = Math.floor(Math.random()*255);
+
+    return "rgb(" + r +  "," + g + "," + b + ")";
+}
+
+function setupSquare(numSqaure){
+    
+    let ans = color[Math.floor(Math.random()*(numSqaure-1))];
+    requiredColor.innerText = ans;
+    for(let i = 0; i < numSqaure; i++){
+        button[i].addEventListener('click', () => {
+            if (color[i] == ans) {
+                result.innerText = "Correct";
+                result.style.color = "green";
+                for (let j = 0; j < numSqaure; j++) {
+                    button[j].style.backgroundColor = ans;
+                }
+                header[0].style.backgroundColor = ans;
+                reset.innerText = "Play Again?"
+            }
+            else{
+                result.innerText = "Try Again";
+                result.style.color = "red";
+                button[i].style.backgroundColor = "rgb(138, 43, 226)";
+            }
+        })
+    }
+}
+
+function generateRandomColors(numSqaure){
+    for (let i = 0; i < numSqaure; i++) {
+        color[i] = randomColor();
+        button[i].style.backgroundColor = color[i];
+    }
+}
+
+reset.addEventListener('click', () => {
+    result.innerText = '';
+    reset.innerText = "New Color";
+    generateRandomColors(numSqaure)
+    setupSquare(numSqaure);
+})
+
+easy.addEventListener('click', () => {
+    result.innerText = '';
+    reset.innerText = "New Color";
+    document.getElementsByClassName("row-2")[0].style.display = "none";
+    numSqaure = 3;
+    generateRandomColors(numSqaure);
+    setupSquare(numSqaure);
+
+})
+
+hard.addEventListener('click', () => {
+    result.innerText = '';
+    reset.innerText = "New Color";
+    document.getElementsByClassName("row-2")[0].style.display = "flex";
+    numSqaure = 6;
+    generateRandomColors(numSqaure)
+    setupSquare(numSqaure);
+
+})
+
+
+
+
+
+
+
+
+
+
+
